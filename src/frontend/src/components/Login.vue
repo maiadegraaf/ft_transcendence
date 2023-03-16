8632f4743fc8b0fd670d9e0dab2aee430b139335
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2023-03-16 13:51:55 +0100

    frontend

---
+++ src/frontend/src/components/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-39cfad1d6f73c8c527bed1273cb6689ff7a806f4fae38fc551c6c2f3aa6cfa44&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&response_type=code">Login</a>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ src/frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Login from '../components/Login.vue'
+import CreatePost from '../components/CreatePost.vue'
+</script>
+
+<template>
+  <div>
+    <header></header>
+    <main>
+      <Login/>
+      <CreatePost/>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  place-items: center;
+}
+</style>
