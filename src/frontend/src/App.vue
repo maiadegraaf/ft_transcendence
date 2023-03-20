95d2ba18dd1e046a3ff7b564ac77c8178fd96aee
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2023-03-20 13:23:19 +0100

    tailwincss

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-link to="/Home" exact>Home</router-link>
+  <nav class="flex justify-center p-3">
+    <router-link class="hover:opacity-70 text-gray-900 transition-opacity ease-in-out" to="/Home" exact>Home</router-link>
+  </nav>
   <router-view/>
 </template>
 
