0261985a15c03a8a62c6ecbaf3186cc21916cefb
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2023-03-20 14:07:22 +0100

    small changes config

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../components/Login.vue'
+import Auth from '../views/Authenticated.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/Home',
       name: 'Home',
       component: () => Home
+    },
+    {
+      path: '/Authenticated',
+      name: 'Authenticated',
+      component: () => Auth
     },
     {
       path: '/',
---
+++ src/frontend/src/views/Authenticated.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Authenticated</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
