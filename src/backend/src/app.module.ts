0a4b84d0d32a486925ef87acaea61f6856ff1888
Alle <allejanse97@gmail.com>
2023-03-21 12:39:56 +0100

    Added database connection and made the first entities

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/auth'),
+    //MongooseModule.forRoot('mongodb://localhost:27017/auth'),
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
