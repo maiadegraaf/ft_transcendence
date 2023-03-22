a038f0a4dca16f370206a4826a8f0410219d9620
Alle <allejanse97@gmail.com>
2023-03-22 15:33:28 +0100

    Change template Chat.vue

--- src/frontend/src/components/Chat.vue
+++ src/frontend/src/components/Chat.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app" class="container">
-            <div class="row">
-                <div class="col-md-6 offset-md-3 col-sm-12">
-                    <h1 class="text-center">{{ title }}</h1>
-                    <br>
-                    <div id="status"></div>
-                    <div id="chat">
-                        <input type="text" v-model="name" id="username" class="form-control" placeholder="Enter name...">
-                        <br>
-                        <div class="card">
-                            <div id="messages" class="card-block">
-                                <ul>
-                                    <li v-for="message of messages" :key="message.name">{{ message.name }}: {{ message.text }}</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <br>
-                        <textarea id="textarea" class="form-control" v-model="text" placeholder="Enter message..."></textarea>
-                        <br>
-                        <button id="send" class="btn" @click.prevent="sendMessage">Send</button>
-                    </div>
-                </div>
-            </div>
-    </div>
+  <v-card
+      class="mx-auto"
+      color="secondary"
+      width="75%"
+      title="Chat"
+  >
+    <v-list bg-color="secondary">
+      <div v-for="message of messages" :key="message.name">
+        <v-card v-if="message.name === 'Bert'"
+                flat
+                tile
+                class="mb-1 ml-2 d-flex justify-center"
+                width=50%
+        >
+          <div class="font-weight-bold mt-2 ml-3"> {{ message.name }} </div>
+          <div class="mr-2 mb-2 ml-3"> {{ message.text }} </div>
+        </v-card >
+        <div class="d-flex justify-end">
+          <v-card v-if="message.name === 'Hans'"
+                  class="mb-1 mr-2 text-right d-flex justify-center"
+                  tile
+                  flat
+                  width=50%
+                  color="red"
+          >
+            <div class="font-weight-bold mr-4 mt-1"> {{ message.name }} </div>
+            <div class="mr-4 mb-1 ml-2"> {{ message.text }} </div>
+          </v-card>
+        </div>
+      </div>
+    </v-list>
+  </v-card>
+  <br>
+  <input type="text" v-model="name" id="username" class="form-control" placeholder="Enter name...">
+  <br>
+  <textarea id="textarea" class="form-control" v-model="text" placeholder="Enter message..."></textarea>
+  <br>
+  <button id="send" class="btn" @click.prevent="sendMessage">Send</button>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import io from 'socket.io-client';
 
 export default {
@@ -37,10 +50,11 @@ export default {
     title: string;
     name: string;
     text: string;
-    messages: { 
-		name: string;
-		text: string;
-	}[];
+    messages: {
+      name: string;
+      text: string;
+      // id: number
+    }[];
     socket: any;
   } {
     // The initial data of the Vue instance.
@@ -79,11 +93,11 @@ export default {
     },
   },
   // The created hook of the Vue instance.
-  created: function (): void {
+  created(): void {
     // Initializes the Socket.IO client and stores it in the Vue instance.
     this.socket = io('http://localhost:8080');
     // Listens for 'msgToClient' events and calls the receivedMessage method with the message.
-    this.socket.on('msgToClient', (message: { name: string; text: string }) => {
+    this.socket.on('msgToClient', (message: { name: string; text: string}) => {
       this.receivedMessage(message);
     });
   },
@@ -92,14 +106,5 @@ export default {
 
 <style>
 
-#messages{
-  height:300px;
-  overflow-y: scroll;
-}
 
-#app {
-  margin-top: 2rem;
-  margin: auto;
-  width: 40%;
-}
 </style>
