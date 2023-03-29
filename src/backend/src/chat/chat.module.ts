1cf1124884b4bcfd45990edbbcc1d326c66782f3
sde-quai <stormdequay@gmail.com>
2023-03-29 12:23:18 +0200

    Congrolleor chat, add group and dm channel and a GroupProfile with service

--- src/backend/src/chat/chat.module.ts
+++ src/backend/src/chat/chat.module.ts
@@ -8,10 +8,19 @@ import { ChatController } from './chat.controler';
 import { ChannelService } from './services/channel.service';
 import { GroupProfile } from './entities/groupProfile.entity';
 import { GroupProfileService } from './services/groupProfile.service';
+// import { UserModule } from '../users/users.module';
+import { User } from '../users/entities/users.entity';
+import { UsersService } from '../users/services/users/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message, Channel, GroupProfile])],
+  imports: [TypeOrmModule.forFeature([Message, Channel, GroupProfile, User])],
   controllers: [ChatController],
-  providers: [ChatGateway, MessageService, ChannelService, GroupProfileService],
+  providers: [
+    ChatGateway,
+    MessageService,
+    ChannelService,
+    GroupProfileService,
+    UsersService,
+  ],
 })
 export class ChatModule {}
