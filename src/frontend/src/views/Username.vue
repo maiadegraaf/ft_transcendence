f2b86cedf4e7e0a7ba68b4201b78c0d6783498f9
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2023-04-25 15:24:35 +0200

    page username

--- src/frontend/src/views/Username.vue
+++ src/frontend/src/views/Username.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col justify-center align-center h-screen">
     <div class="flex flex-col align-center justify-center">
-      <label class="text-5xl mb-12 font-semibold">Choose your username:</label>
+      <label class="text-5xl mb-8 font-semibold">Choose your username:</label>
+      <p class="h-1 mb-4 p-2 font-bold text-blush">{{ error }}</p>
       <input type="text" v-model="newUserName" class="border rounded mt-3"/>
     </div>
     <button @click="changeUsername" class="text-xl p-2 font-semibold hover:border-amaranth-purple hover:text-amaranth-purple tracking-wider text-blush drop-shadow-2xl uppercase">confirm</button>
@@ -16,6 +17,7 @@ export default {
   data() {
     return {
       newUserName: '',
+      error: '',
       user: {
         id: Number,
         login: '',
@@ -28,16 +30,17 @@ export default {
   },
   methods: {
     changeUsername() {
-      axios
-          .post('/api/user/username', {
-            username: this.newUserName,
-          })
-          .then((response) => {
-            if (response.data)
+      if (this.newUserName.trim().length === 0)
+        this.error = "The username can't be empty !"
+      else
+      {
+        axios.post('/api/user/username', {
+          username: this.newUserName,
+        })
+            .then((response) => {
               this.$router.push('/Home');
-            else
-              this.error = "Something went wrong!"
-          })
+            })
+      }
     },
     pushHome() {
       this.$router.push('/Home')
