e49164999b92fad5d1d7e3299886ef8c9cee4688
pderksen98 <pieter.derksen@hotmail.com>
2023-05-01 13:23:20 +0200

    small change

--- src/frontend/src/components/profile/friends.vue
+++ src/frontend/src/components/profile/friends.vue
@@ -29,6 +29,11 @@ export default defineComponent({
                 this.searchError = null;
             } catch (error) {
                 console.error("Failed to add friend", error);
+                if (typeof error === "string") {
+                    this.searchError = error;
+                } else {
+                    this.searchError = "Failed to add friend";
+                }
             }
         },
         async searchForUser() {
@@ -50,8 +55,7 @@ export default defineComponent({
 });
 </script>
 
-
-<template>
+<!-- <template>
     <div class="flex flex-col justify-center items-center mt-16">
         <div class="border-double border-4 text-buff relative border-buff rounded-md w-[50vw] min-w-[600px] pt-10 p-4">
             <h1
@@ -84,10 +88,8 @@ export default defineComponent({
                     </h3>
                 </div>
             </div>
-            <!-- ******** UGGLY BUT JUST TO CHECK IF IT WORKS ******* -->
             <h2>Search for friends</h2>
             <div class="search-section">
-                <!-- <input type="text" v-model="searchInput" @input="searchForUser" /> -->
                 <input type="text" v-model="searchInput" @keyup.enter="searchForUser" />
                 <div v-if="searchResult">
                     <span>{{ searchResult.login }}</span>
@@ -97,9 +99,23 @@ export default defineComponent({
                     <span>{{ searchError }}</span>
                 </div>
             </div>
-            <!-- ****************************************************** -->
         </div>
     </div>
+</template> -->
+
+
+
+
+
+<template>
+  <div class="flex flex-col justify-center items-center mt-16">
+      <div class="border-double border-4 text-buff relative border-buff rounded-md w-[50vw] min-w-[600px] pt-10 p-4">
+          <h1
+              class="text-2xl font-bold text-white absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-dark-purple rounded-md border-2 border-buff px-4 py-2">
+              Friends
+          </h1>
+      </div>
+  </div>
 </template>
 
 
@@ -117,34 +133,3 @@ export default defineComponent({
     @apply text-xl text-white font-bold mb-4;
 }
 </style>
-
-
-
-<!-- ********* ALFREDS SCRIPT FOR WIN/LOSS ********* -->
-<!-- 
-<script lang="ts">
-import axios from 'axios'
-export default {
-    name: 'WinLosses',
-    data() {
-        return {
-            currentUserId: 0,
-            scoreData: {
-                rating: 0,
-                wins: 0,
-                losses: 0,
-                winStreak: 0
-            }
-        }
-    },
-    async mounted() {
-        await axios.get('/api/auth/profile').then((response) => {
-            this.currentUserId = response.data.id
-        })
-        axios.get('/api/leaderboard/' + this.currentUserId).then((response) => {
-            this.scoreData = response.data
-        })
-        console.log(this.scoreData)
-    }
-}
-</script> -->
