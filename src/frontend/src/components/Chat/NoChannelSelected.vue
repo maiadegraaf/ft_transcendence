10a943960a6cbb79eeb4496bbfbc4bdcef973c9b
Ajanse568 <allejanse97@gmail.com>
2023-05-11 08:43:37 +0200

    Cleaned package.json

---
+++ src/frontend/src/components/Chat/NoChannelSelected.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import {ChatBubbleLeftRightIcon} from "@heroicons/vue/24/outline";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "NoChannelSelected",
+    components: {
+        ChatBubbleLeftRightIcon,
+    },
+})
+</script>
+
+<template>
+    <div class="flex-1 flex justify-center w-full bg-dark-purple overflow-hidden">
+        <div class="my-auto">
+            <ChatBubbleLeftRightIcon class="h-40 w-40 text-buff mx-auto"/>
+            <h1 class="text-center text-3xl text-buff">Select a channel to start chatting</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
